49b06f47addfa2c51ae92f47d7c5006bd3320a09
YuvrajRajbanshi <singhraj0461@gmail.com>
2025-02-03 23:24:36 +0530

    Creating a tree and finding all the neighbors of particular vertex

---
+++ Operation/Classroom.java
@@ -0,0 +1,52 @@
+package Operation;
+
+import java.util.ArrayList;
+
+public class Classroom {
+
+    public static class Edge {
+        int src; // source of the graph
+        int dest; // destination
+        int wt; // weight of the edge
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int v = 5;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+
+        // 0 - vertex
+        graph[0].add(new Edge(0, 1, 5));
+
+        // 1- vertex
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+
+        // 2 vertex
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 4, 4));
+
+        // 3 Vertex
+        graph[3].add(new Edge(3, 1, 3));
+        graph[3].add(new Edge(3, 2, 1));
+
+        // 4 vertex
+        graph[4].add(new Edge(4, 2, 2));
+
+        // printing 2's neighbors
+        for (int i = 0; i < graph[2].size(); i++) {
+            Edge e = graph[2].get(i);
+            System.out.print(e.dest);
+        }
+
+    }
+}
