a56506ff74cd160568407839f2d3636799b91b4b
YuvrajRajbanshi <singhraj0461@gmail.com>
2025-02-05 22:32:36 +0530

    hasPath

---
+++ HasPath/Haspath.java
@@ -0,0 +1,74 @@
+package HasPath;
+
+import java.util.*;
+
+public class Haspath {
+
+    public static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        // vertext 0
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+        // vertext 1
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+        // vertext 2
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+        // vertext 3
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+        // vertext 4
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+        // vertext 5
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+        // vertext 6
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    public static boolean hasRasta(ArrayList<Edge>[] graph, int src, int dest, boolean vis[]) {
+        if (src == dest) {
+            return true;
+        }
+        vis[src] = true;
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            // e.dest = neighbour
+            if (!vis[e.dest] && hasRasta(graph, e.dest, dest, vis)) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+
+        int V = 7;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        boolean vis[] = new boolean[graph.length];
+
+        System.out.println(hasRasta(graph, 0, 10, vis));
+    }
+}
--- Operation/Classroom.java
+++ Operation/Classroom.java
@@ -14,45 +14,37 @@ public class Classroom {
             this.dest = dest;
             this.wt = wt;
         }
-
     }
 
     public static void main(String[] args) {
-
         int v = 5;
         @SuppressWarnings("unchecked")
+        // int[] hell = new int[5]; just see 22 and 23 line maga
         ArrayList<Edge>[] graph = new ArrayList[v];
 
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
-
         // 0 - vertex
         graph[0].add(new Edge(0, 1, 5));
-
         // 1- vertex
         graph[1].add(new Edge(1, 0, 5));
         graph[1].add(new Edge(1, 2, 1));
         graph[1].add(new Edge(1, 3, 3));
-
         // 2 vertex
         graph[2].add(new Edge(2, 1, 1));
         graph[2].add(new Edge(2, 3, 1));
         graph[2].add(new Edge(2, 4, 4));
-
         // 3 Vertex
         graph[3].add(new Edge(3, 1, 3));
         graph[3].add(new Edge(3, 2, 1));
-
         // 4 vertex
         graph[4].add(new Edge(4, 2, 2));
-
         // printing 2's neighbors
         for (int i = 0; i < graph[2].size(); i++) {
             Edge e = graph[2].get(i);
             System.out.print(e.dest);
         }
         System.out.println();
-
     }
 }
